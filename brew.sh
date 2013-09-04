879116bbfa7c6b1b2f26be6bd5be05f7c7ae39df
tim.tang <tang.jilong@139.com>
2013-09-04 08:25:55 +0200

    [dotfile] - use brew.sh to install native application automatic.

--- brew.sh
+++ brew.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Make sure we’re using the latest Homebrew
 brew update
@@ -9,6 +9,7 @@ brew upgrade
 # Install GNU core utilities (those that come with OS X are outdated)
 brew install coreutils
 echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
+
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
 brew install findutils
 # Install Bash 4
@@ -17,32 +18,40 @@ brew install bash
 # Install wget with IRI support
 brew install wget --enable-iri
 
-# Install RingoJS and Narwhal
-# Note that the order in which these are installed is important; see http://git.io/brew-narwhal-ringo.
-#brew install ringojs
-#brew install narwhal
-
 # Install more recent versions of some OS X tools
 brew tap homebrew/dupes
 brew install homebrew/dupes/grep
-brew tap josegonzalez/homebrew-php
-brew install php54
 
-# These two formulae didn’t work well last time I tried them:
-#brew install homebrew/dupes/vim
-#brew install homebrew/dupes/screen
-
-# Install everything else
+# Install other useful binaries
+brew install wget
 brew install ack
-#brew install exiv2
 brew install git
-#brew install imagemagick
+brew install imagemagick
 brew install lynx
-brew install node
+#brew install node
+brew install pigz
 brew install rename
-brew install rhino
 brew install tree
-brew install webkit2png
+
+# Install native apps
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+function installcask() {
+    brew cask install "${@}" 2> /dev/null
+}
+
+installcask evernote
+installcask google-chrome
+installcask google-chrome-canary
+installcask iterm2
+installcask miro-video-converter
+installcask vlc
+installcask u-torrent
+installcask qq
+installcask skitch
+installcask the-unarchiver
+installcask dropbox
 
 # Remove outdated versions from the cellar
 brew cleanup
