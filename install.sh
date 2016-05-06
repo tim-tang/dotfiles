deccdc79904d25c60e738d5d59b0e29ea91b9248
Tim.Tang <tang.jilong@139.com>
2016-05-06 18:45:26 +0800

    [dotfiles] - Do house keeping.

--- install.sh
+++ install.sh
@@ -8,14 +8,4 @@ done
 cp -r -f $PWD/gnzh.zsh-theme $HOME/.oh-my-zsh/themes/
 cd $HOME && source $HOME/.zshrc
 ./.osx
-
-echo "-- Install coreutils"
-wget http://ftp.gnu.org/gnu/coreutils/coreutils-8.21.tar.xz
-tar xvJf coreutils-8.21.tar.xz
-cd coreutils-8.21
-wget http://zwicke.org/web/advcopy/advcpmv-0.5-8.21.patch
-patch -p1 -i advcpmv-0.5-8.21.patch$ ./configure$ make
-sudo cp src/cp /bin/cp
-sudo cp src/mv /bin/mv
-
 echo "-- Re-configure mac os environment successfully --"
